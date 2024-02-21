f7146543623e000f0809f0495aacf5e13cb7ead9
AliAksoyy <alifeyza4017@gmail.com>
2024-02-21 15:10:45 +0300

    added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const http = require("http");
 const dotenv = require("dotenv");
 const app = require("./app");
 const path = require("path");
-
 dotenv.config({ path: path.join(__dirname, "config", ".env") });
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
@@ -12,12 +12,19 @@ const server = http.createServer(app);
 mongoose.connect(process.env.MONGO_URI);
 
 mongoose.connection.on("connected", () => {
-  console.log("MongoDb successfully connected");
+  console.log("mongodb connection successful");
+  startServer();
 });
 mongoose.connection.on("disconnect", () => {
   console.log("MongoDb  connected rejected");
 });
 
-server.listen(PORT, () => {
-  console.log(`http:/localhost:${PORT} listening`);
+function startServer() {
+  if (!server.listening) {
+    server.listen(process.env.PORT);
+  }
+}
+
+server.on("listening", () => {
+  console.log(`Chat App start on http://localhost:${PORT}`);
 });
